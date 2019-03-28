baff058bc7a43cd6d0877d84b084e5e9b1d3088b
gfike19@gmail.com <Vaughn*573>
2019-03-28 15:30:26 -0500

    did things

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+def removeWS(word):
+    return word.replace(" ", "")
+
 fileList = open("yuge-list.txt", "r+")
 
 lst = []
@@ -6,13 +9,20 @@ for line in fileList:
 fileList.close()
 cleanList = []
 
-idx = 0
+# idx = 0
 
-while idx < len(lst) - 1:
-    for line in lst:
-        if "\n" not in line or lst[idx] not in lst[idx + 1]:
-            cleanList.append(line)
-    idx += 1
+# while idx < len(lst) - 1:
+#     for line in lst:
+#         if "\n" not in line.strip() or lst[idx].strip() not in lst[idx + 1].strip():
+#             cleanList.append(line)
+#     idx += 1
+
+for each in lst:
+    if each != "\n" or each != "\r":
+        if removeWS(each) != removeWS(next(iter(lst))):
+            cleanList.append(each)
+
+cleanList.sort()
 
 newFile = open("test.txt", "a+")
 
